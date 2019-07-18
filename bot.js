2c3a72c5f233f97900abfe8659bcb42d0d970f4b
Keiran Young <kcyoung1997@hotmail.co.uk>
2019-07-18 18:39:46 +0100

    Added Winston logging

--- bot.js
+++ bot.js
@@ -1,29 +1,49 @@
-var Discord = require('discord.js');
+var Discord = require('discord.js')
 var auth = require('./auth.json');
-console.log('Init');
+var winston = require('winston')
+var format =  winston.format.combine(
+    winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+)
+var logger = new winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            json: false,
+            format: winston.format.combine(winston.format.colorize(), format)
+        }),
+        new winston.transports.File({
+            filename: './bot.log',
+            format: format
+        })
+    ]
+})
 // Initialize Discord Bot
-const bot = new Discord.Client();
+const bot = new Discord.Client()
+
+require('death')((signal, err) => {
+    logger.info('Program exiting')
+    bot.destroy()
+    logger.info('Logged out')
+})
+
 
 bot.on('ready', function() {
-    console.log('Logged in as %s\n', bot.user.tag);
-});
+    logger.info('Logged in as ' + bot.user.tag);
+})
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
   // Check if user has moved to a new voice channel.
-  if(newMember.voiceChannel){
+  if(newMember.voiceChannel && oldMember.voiceChannel && (newMember.voiceChannel != oldMember.voiceChannel)){
+    logger.info(newMember.displayName + ' joined ' + newMember.voiceChannel.name)
     // Get Username based on PartyBeast's "Username's Channel" format
-    var owner = newMember.voiceChannel.name.split("'")[0];
-    console.log('owner: '+owner);
-    // Get User's name
-    var name = newMember.displayName;
-    console.log('name: '+name);
+    var owner = newMember.voiceChannel.name.split("'")[0]
     // If user is the one the channel was created for
-    if(owner === name) {
-      console.log('Owner, adding perms: '+name);
+    if(owner === newMember.displayName) {
+      logger.info('Adding perms for '+newMember.displayName);
       // Give some perms
-      newMember.voiceChannel.overwritePermissions(newMember.id, {'MANAGE_CHANNELS': true, 'MANAGE_ROLES': true});
+      newMember.voiceChannel.overwritePermissions(newMember.id, {'MANAGE_CHANNELS': true, 'MANAGE_ROLES': true})
     }
   }
 })
 
-bot.login(auth.token);
+bot.login(auth.token)
